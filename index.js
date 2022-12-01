2d23f1687314bd61abe12792c3f2247e0d835805
Brianna Wilson <bronna@gmail.com>
2022-12-01 15:09:37 -0800

    Updated home page layout

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 function rainbowLava(section) {
-    let topMargin, btmMargin, part, partHeight, parterHeight
-    
-    if (section === "lavaHeader") {
-        topMargin = 192
-        btmMargin = 64
-        part = document.querySelector("#header-main")
-        partHeight = part.offsetHeight
-        //parterHeight = topMargin + partHeight + btmMargin
-        parterHeight = window.innerHeight
-    } else if (section === "lavaFooter") {
-        topMargin = 128
-        btmMargin = 64
-        part = document.querySelector("#about")
-        partHeight = part.offsetHeight
-        parterHeight = topMargin + partHeight + btmMargin
-    }
-  
     /**
      * Setup
      */
@@ -45,8 +28,7 @@ function rainbowLava(section) {
     camera.lookAt(0, 0, -20)
   
     const renderer = new THREE.WebGLRenderer()
-    //renderer.setSize( window.innerWidth, (window.innerHeight / 5) * 4 )
-    renderer.setSize( window.innerWidth, parterHeight)
+    renderer.setSize( window.innerWidth, window.innerHeight )
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     const container = document.getElementById(section)
     container.appendChild(renderer.domElement)
@@ -73,7 +55,7 @@ function rainbowLava(section) {
         geometry = new THREE.PlaneBufferGeometry(width, height, widthSegments, heightSegments)
   
         glowMaterial = new THREE.ShaderMaterial({
-            vertexShader: document.getElementById('lava-vertex').textContent,
+            vertexShader: document.getElementById('vertex').textContent,
             fragmentShader: document.getElementById('lava-fragment').textContent,
             uniforms: {
                 uTime: { value: 0 },
@@ -114,18 +96,173 @@ function rainbowLava(section) {
      * Handle Resize
      */
     function onWindowResize() {
-        partHeight = part.offsetHeight
-        parterHeight = topMargin + partHeight + btmMargin
-
-        //camera.aspect = window.innerWidth / ((window.innerHeight / 5) * 4)
-        camera.aspect = window.innerWidth / parterHeight
+        camera.aspect = window.innerWidth / window.innerHeight
         camera.updateProjectionMatrix()
-  
-        //renderer.setSize(window.innerWidth, (window.innerHeight / 5) * 4)
-        renderer.setSize(window.innerWidth, parterHeight)
+
+        renderer.setSize(window.innerWidth, window.innerHeight)
   
         generateObjects()
     }
+
+    // /**
+    //  * Debug
+    //  */
+    // const debugObject = {}
+
+    // /**
+    //  * Setup
+    //  */
+    // const scene = new THREE.Scene()
+
+    // const parameters = {}
+    // parameters.cameraX = -300
+    // parameters.cameraY = 400
+    // parameters.cameraZ = 330
+    // parameters.count = 300000
+    // parameters.size = 1.0
+
+    // const camera = new THREE.PerspectiveCamera(
+    //     50,
+    //     window.innerWidth / window.innerHeight,
+    //     0.1,
+    //     1000
+    // )
+    // camera.position.set(parameters.cameraX, parameters.cameraY, parameters.cameraZ)
+    // camera.up.set(0, 1, 0)
+    // camera.lookAt(-80, 0, -80)
+
+    // const renderer = new THREE.WebGLRenderer({ antialias: true })
+    // renderer.setSize( window.innerWidth, window.innerHeight )
+    // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    // //document.body.appendChild(renderer.domElement)
+    // const container = document.getElementById(section)
+    // container.appendChild(renderer.domElement)
+
+    // /**
+    //  * Graphics
+    //  */
+    // let width = 1400
+    // let height = 2000
+    // let widthSegments = 400
+    // let heightSegments = 800
+    // let geometry = null
+    // let sandGeometry = null
+    // let glowMaterial = null
+    // let sandMaterial = null
+    // let glow = null
+    // let sands = null
+
+    // // Colors
+    // debugObject.depthColor = '#a2dcc6'
+    // debugObject.surfaceColor = '#ffbea4'
+
+    // const generateObjects = () => {
+    //     // Destroy old objects
+    //     if(sands !== null) {
+    //         sandGeometry.dispose()
+    //         sandMaterial.dispose()
+    //         scene.remove(sands)
+    //     }
+
+    //     if(glow !== null) {
+    //         geometry.dispose()
+    //         glowMaterial.dispose()
+    //         scene.remove(glow)
+    //     }
+
+    //     geometry = new THREE.PlaneBufferGeometry(width, height, widthSegments, heightSegments)
+
+    //     // sandGeometry = new THREE.BufferGeometry()
+    //     // const positions = new Float32Array(parameters.count * 3)
+    //     // for (let i = 0; i < parameters.count; i++) {
+    //     //     const i3 = i * 3
+
+    //     //     positions[i3    ] = (Math.random() - 0.5) * width
+    //     //     positions[i3 + 1] = (Math.random() - 0.5) * height
+    //     //     positions[i3 + 2] = 0
+
+    //     //     sandGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    //     // }
+
+    //     glowMaterial = new THREE.ShaderMaterial({
+    //         vertexShader: document.getElementById('vertex').textContent,
+    //         fragmentShader: document.getElementById('glow-fragment').textContent,
+    //         uniforms: {
+    //             uTime: { value: 0 },
+    //             uSpeed: { value: 0.5 },
+    //             uMountainFrequency: { value: 300.0 },
+    //             uMountainHeight: { value: 140.0 },
+    //             uPallete: { type: 't', value: null },
+    //             uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+    //             uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+    //             uColorOffset: { value: 0.8 },
+    //             uColorMultiplier: { value: 0.9 },
+    //         }
+    //     })
+        
+    //     // sandMaterial = new THREE.ShaderMaterial({
+    //     //     depthWrite: false,
+    //     //     blending: THREE.AdditiveBlending,
+    //     //     vertexColors: true,
+    //     //     vertexShader: document.getElementById('particle-vertex').textContent,
+    //     //     fragmentShader: document.getElementById('particle-fragment').textContent,
+    //     //     uniforms: {
+    //     //         uTime: { value: 0 },
+    //     //         uSpeed: { value: 0.5 },
+    //     //         uMountainFrequency: { value: 4.2 },
+    //     //         uMountainHeight: { value: 100.0 },
+    //     //         uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+    //     //         uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+    //     //         uColorOffset: { value: 0.8 },
+    //     //         uColorMultiplier: { value: 0.9 },
+    //     //         uSize: { value: parameters.size * renderer.getPixelRatio() }
+    //     //     }
+    //     // })
+
+    //     glow = new THREE.Mesh( geometry, glowMaterial)
+    //     glow.position.z = -220
+    //     glow.rotation.x = -Math.PI / 2
+    //     scene.add( glow )
+
+    //     // sands = new THREE.Points( sandGeometry, sandMaterial )
+    //     // sands.position.z = -220
+    //     // sands.rotation.x = -Math.PI / 2
+    //     // scene.add( sands )
+
+    //     // Resize
+    //     window.addEventListener('resize', onWindowResize)
+    // }
+
+    // generateObjects()
+
+    // /**
+    //  * Animate
+    //  */
+    // const clock = new THREE.Clock()
+
+    // function animate() {
+    //     const elapsedTime = clock.getElapsedTime()
+
+    //     glowMaterial.uniforms.uTime.value = elapsedTime
+    //     // sandMaterial.uniforms.uTime.value = elapsedTime
+
+    //     requestAnimationFrame( animate )
+
+    //     renderer.render( scene, camera )
+    // }
+    // animate()
+
+    // /**
+    //  * Handle Resize
+    //  */
+    // function onWindowResize() {
+    //     camera.aspect = window.innerWidth / window.innerHeight
+    //     camera.updateProjectionMatrix()
+
+    //     renderer.setSize(window.innerWidth, window.innerHeight)
+
+    //     generateObjects()
+    // }
 }
 rainbowLava('lavaHeader')
 rainbowLava('lavaFooter')
