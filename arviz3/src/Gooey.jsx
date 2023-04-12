4e30d8b02b367e269139bcd39d75a9afb8741487
Brianna Wilson <bronna@gmail.com>
2023-04-12 11:51:03 -0700

    Update arviz directory structure

--- arviz3/src/Gooey.jsx
+++
@@ -1,8 +0,0 @@
-import GooeyEffect from "./GooeyEffect"
-import { forwardRef } from "react"
-
-export default forwardRef(function Gooey(props, ref) {
-    const effect = new GooeyEffect(props)
-
-    return <primitive ref={ ref } object={ effect } />
-})
