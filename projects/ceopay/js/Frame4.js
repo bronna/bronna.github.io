b326351d0c47acc4d5d7903f9f1df02c77129e45
Brianna Wilson <bronna@gmail.com>
2023-06-26 12:18:11 -0700

    Updated entire portfolio layout

---
+++ projects/ceopay/js/Frame4.js
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////
+////Starts transition to work hours chart
+//References: 
+//    https://github.com/nbremer/Chord-Diagram-Storytelling
+
+async function Frame4() {
+
+  //Show & run progress bar
+    runProgressBar(time=700*3);
+    
+  //Fade the background
+    fadeBG.transition().duration(fade).attr("opacity", 0.9)
+    
+  //Remove the previous caption
+    bounds.selectAll(".caption").transition().duration(fade)
+        .attr("opacity", 0)
+    
+  //Change the text
+    changeTopText(newText = "To put things into perspective,",
+	loc = 4/2, delayDisappear = 0, delayAppear = 1, finalText = true, xloc = dimensions.boundedWidth / 2, w = 350);
+    
+  //Add bottom text
+    changeBottomText(newText = "let's compare this to hours worked",
+	loc = 1/2, delayDisappear = 0, delayAppear = 3, finalText = true, xloc = dimensions.boundedWidth / 2, w = 350);
+    
+}
---
+++ scripts.js
@@ -0,0 +1,9 @@
+function navigate(event) {
+    if (event.target.classList.contains('nested-link')) {
+        // If the nested link was clicked, allow the default action (the navigation to the href of the link)
+    } else {
+        // Otherwise, prevent the default action and navigate to the project page
+        event.preventDefault();
+        window.location.href = 'https://observablehq.com/@bronna/parametric-seashell';
+    }
+}
